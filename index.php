0fff21265090df01bc6bbd908961397232df5ae6
Sami Gilliand <sami.gilliand@heig-vd.ch>
2023-10-06 11:59:58 +0200

    Modification d'erreur : Changement de la BASE_URL en 127.0.0.1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', 'http://localhost/comem-archidep-php-todo-exercise/');
+define('BASE_URL', 'http://127.0.0.1/comem-archidep-php-todo-exercise/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
