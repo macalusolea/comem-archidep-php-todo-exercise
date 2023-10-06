89e02e14138867073bda8f8fa9d99a9e27128a5e
Lea Macaluso <lea.macaluso@heig-vd.ch>
2023-10-06 11:45:03 +0200

    troisieme implement me: selection des taches et ordres descendants

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $deleteQuery = 'DELETE FROM todo WHERE id = ' . $id; // IMPLEMENT ME
+        $deleteQuery = 'DELETE FROM todo WHERE id = ' . $id;
         if(!$db->query($deleteQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'SELECT * FROM todo ORDER BY creation_date DESC'; 
 $items = $db->query($selectQuery);
 ?>
 
